1f95f22ab5d077db2beb06a870ec5d223e0ef0a1
Kristina <59663597+KristinaFrants@users.noreply.github.com>
2020-07-07 16:49:13 -0400

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -5,12 +5,12 @@ import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-var currentTab = 0; // Current tab is set to be the first tab (0)
-showTab(currentTab); // Display the current tab
+let pmProfileTab = 0; // pmProfileTab tab is set to be the first tab (0)
+showFormPage(pmProfileTab); // Display the pmProfileTab tab 
 
-function showTab(n) {
+function showFormPage(n) {
   // This function will display the specified tab of the form ...
-  var x = document.getElementsByClassName("tab");
+  let x = document.getElementsByClassName("tab");
   x[n].style.display = "block";
   // ... and fix the Previous/Next buttons:
   if (n == 0) {
@@ -20,6 +20,7 @@ function showTab(n) {
   }
   if (n == x.length - 1) {
     document.getElementById("nextBtn").innerHTML = "Submit";
+
   } else {
     document.getElementById("nextBtn").innerHTML = "Next";
   }
@@ -27,33 +28,54 @@ function showTab(n) {
   fixStepIndicator(n);
 }
 
-window.nextPrev = function nextPrev(n) {
+function nextPrev(n) {
   // This function will figure out which tab to display
-  var x = document.getElementsByClassName("tab");
+  let x = document.getElementsByClassName("tab");
   // Exit the function if any field in the current tab is invalid:
   if (n == 1 && !validateForm()) return false;
   // Hide the current tab:
-  x[currentTab].style.display = "none";
+  if (pmProfileTab < x.length-1) {
+    x[pmProfileTab].style.display = "none";  
+  }
   // Increase or decrease the current tab by 1:
-  currentTab = currentTab + n;
+  pmProfileTab = pmProfileTab + n;
   // if you have reached the end of the form...
-  if (currentTab >= x.length) {
+  if (pmProfileTab >= x.length) {
     // ... the form gets submitted:
-    document.getElementById("regForm").submit();
-    return false;
+  var formData = new FormData();
+  $('.user-edit-input').each(function () {
+      formData.append($(this).attr('name'), $(this).val());
+  })
+  $.ajax({
+  type: 'Post',
+  contentType: false,
+  processData: false,
+  url: "{{url_for('pm_profile')}}",
+  data: formData,
+  success: function (data){
+    if (data.success) {
+      window.location.href="{{url_for('pm.dashboard')}}"
+    } else {
+      alert(data.err);
+    }
+  },
+  error: function(){
+    alert('There was a problem communicating with the server.');
+  }
+  });
+  return false;
   }
   // Otherwise, display the correct tab:
-  showTab(currentTab);
+  showFormPage(pmProfileTab);
+ 
 };
 
 function validateForm() {
   // This function deals with validation of the form fields
-  var x,
-    y,
-    i,
-    valid = true;
+  let x, y, i, valid = true;
   x = document.getElementsByClassName("tab");
-  y = x[currentTab].getElementsByTagName("input");
+  //y = x[currentTab].getElementsByTagName("input");
+  y = x[pmProfileTab].querySelectorAll(".needValidation");
   // A loop that checks every input field in the current tab:
   for (i = 0; i < y.length; i++) {
     // If a field is empty...
@@ -66,14 +88,14 @@ function validateForm() {
   }
   // If the valid status is true, mark the step as finished and valid:
   if (valid) {
-    document.getElementsByClassName("step")[currentTab].className += " finish";
+    document.getElementsByClassName("step")[pmProfileTab].className += " finish";
   }
-  return valid; // return the valid status
+  return valid; // return the valid status  
 }
-
+  
 function fixStepIndicator(n) {
   // This function removes the "active" class of all steps...
-  var i,
+  let i,
     x = document.getElementsByClassName("step");
   for (i = 0; i < x.length; i++) {
     x[i].className = x[i].className.replace(" active", "");
@@ -81,3 +103,4 @@ function fixStepIndicator(n) {
   //... and adds the "active" class to the current step:
   x[n].className += " active";
 }
+
